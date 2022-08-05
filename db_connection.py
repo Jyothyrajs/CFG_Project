913b4c6bff71bb55d14851f8158fff92bde171c9
Naomi McEntegart <104771053+naomicodesthings@users.noreply.github.com>
2022-08-05 20:10:12 +0100

    Update db_connection.py
    Added update function so that we can update priority of each task. Code needs checking for mySQL / SQL lite

--- db_connection.py
+++ db_connection.py
@@ -1,18 +1,24 @@
 import mysql.connector
 from config import HOST,USER,PASSWORD
 
+class DbConnectionError(Exception):
+    pass
+
 
 #NOTSTARTED = 'Not Started'
 #INPROGRESS = 'In Progress'
 #COMPLETED = 'Completed'
 
 
-def connect_db(db_name):
-    context = mysql.connector.connect(host=HOST,
-                                      user=USER,
-                                      password=PASSWORD,
-                                      database=db_name)
-    return context
+def _connect_to_db(db_name):
+    cnx = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        auth_plugin='mysql_native_password',
+        database=db_name)
+    return cnx
+
 
 
 
@@ -28,3 +34,27 @@ def insert_new_task(Task,Priority,duration,deadline):
     finally:
         if project_db:
             project_db.close()
+
+# UPDATE priority level of to do list item (e.g. high priority) 
+
+def update_priority(task, priority):
+    # Check if the passed status is a valid value
+    if (priority.lower().strip() == 'High'):
+        status = NOTSTARTED
+    elif (priority.lower().strip() == 'Medium'):
+        status = INPROGRESS
+    elif (priority.lower().strip() == 'Low'):
+        status = COMPLETED
+    else:
+        print("Error: Status invalid " + status)
+        return None
+
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('update items set status=? where item=?', (status, item))
+        conn.commit()
+        return {item: status}
+    except Exception as e:
+        print('Error: ', e)
+        return None
