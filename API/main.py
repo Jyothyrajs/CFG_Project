8b7a0a8929886c9850ba1a60e5e5f98ca85b8c7a
Lucianna Tomlin-Paul <68015823+ldctp@users.noreply.github.com>
2022-08-12 10:11:03 +0100

    Update main.py

--- API/main.py
+++ API/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request # Added render_template for HTML file
 import API.db_connection as dbconnection
 import json
 
+#Starting User Points
 user_points = 0
 
 app =Flask(__name__)
@@ -10,17 +11,17 @@ app =Flask(__name__)
 def index():
     return render_template('base.html') # Links to base.html file in "Templates" directory
 
-@app.route('/item/new', methods=['POST'])
+@app.route('/tasks/new', methods=['POST'])
 def insert_new_task():
     req_data = request.get_json()
-    item = req_data['item']
+    item = req_data['task']
     
     res_data = dbconnection.connect_db(Productivity)
     
-    # return 400 status if item is not added (error)
+    # return 400 status if task is not added (error)
     
     if res_data is None:
-        response = Response("{'error': 'No item added - " + item + "'}", status=400, mimetype='application/json') 
+        response = Response("{'error': 'No task added - " + task + "'}", status=400, mimetype='application/json') 
         return response
     
     #json.dumps to convert python into JSON object
@@ -28,10 +29,10 @@ def insert_new_task():
  
     return response
  
-@app.route('/items/all')
-def retrieve_items():
+@app.route('/tasks/all')
+def retrieve_tasks():
     # Get items from the db connection file 
-    res_data = dbconnection.retrieve_items()
+    res_data = dbconnection.retrieve_tasks()
 
     # Return response
     response = Response(json.dumps(res_data), mimetype='application/json')
@@ -52,17 +53,17 @@ def retrieve_task():
 
 # Function below to retrieve INDIVIDUAL items:
 
-@app.route('/item/status', methods=['GET'])
-def retrieve_item():
+@app.route('/tasks/status', methods=['GET'])
+def retrieve_task():
     # Get parameter from the URL
-    item_name = request.args.get('name')
+    task_name = request.args.get('task')
 
     # Get items from the db_connection file
-    status = dbconnection.retrieve_item(item_name)
+    status = dbconnection.retrieve_task(task_name)
 
     # Return 404 if item not found
     if status is None:
-        response = Response("{'error': 'No item added - %s'}"  % item_name, status=404 , mimetype='application/json')
+        response = Response("{'error': 'No task added - %s'}"  % task_name, status=404 , mimetype='application/json')
         return response
 
     # Return status
@@ -75,7 +76,7 @@ def retrieve_item():
     
     # PUT method to execute the Update_priority function 
     
-@app.route('/item/update', methods=['PUT'])
+@app.route('/tasks/update', methods=['PUT'])
 def update_priority():
     # Get item from the POST body
     req_data = request.get_json()
@@ -87,7 +88,7 @@ def update_priority():
 
     # Return error if the status could not be updated
     if res_data is None:
-        response = Response("{'error': 'Error updating item - '" + item + ", " + status   +  "}", status=400 , mimetype='application/json')
+        response = Response("{'error': 'Error updating task - '" + task + ", " + status   +  "}", status=400 , mimetype='application/json')
         return response
 
     # Return response
@@ -97,7 +98,7 @@ def update_priority():
 
 # DELETE request:
 
-@app.route('/item/remove', methods=['DELETE'])
+@app.route('/task/remove', methods=['DELETE'])
 def delete_task():
     # Get item from the POST body
     req_data = request.get_json()
